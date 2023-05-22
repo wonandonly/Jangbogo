70252cfc2ff20ea28033e19217cc31d0dbf3c588
{wonhee} <wanny416@sookmyung.ac.kr>
2023-05-22 21:39:34 +0900

    [FEAT] chatGPT까지 넣음 근데 응답 에러

--- app/src/main/java/com/example/part2_1_firstactivity/MainActivity4.java
+++ app/src/main/java/com/example/part2_1_firstactivity/MainActivity4.java
@@ -1,8 +1,5 @@
 package com.example.part2_1_firstactivity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Intent;
 import android.os.Build;
@@ -10,12 +7,38 @@ import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import adapter.MessageAdapter;
+import model.Message;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+//import android.telecom.Call;
 
 public class MainActivity4 extends AppCompatActivity {
 
@@ -25,20 +48,66 @@ public class MainActivity4 extends AppCompatActivity {
     TextView textView;
     final int PERMISSION = 1;
 
+    RecyclerView recycler_view;
+    TextView tv_welcome;
+    EditText et_msg;
+    ImageButton btn_send;
+
+    List<Message> messageList;
+    MessageAdapter messageAdapter;
+
+    public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
+    OkHttpClient client;
+
+    private static final String MY_SECRET_KEY = "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";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        sttBtn = findViewById(R.id.imageBtn);
+        btn_send = findViewById(R.id.btn_send);
 
-    // 퍼미션 체크
+        recycler_view = findViewById(R.id.recycler_view);
+        tv_welcome = findViewById(R.id.tv_welcome);
+        et_msg = findViewById(R.id.et_msg);
+
+
+        recycler_view.setHasFixedSize(true);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setStackFromEnd(true);
+        recycler_view.setLayoutManager(manager);
+
+        messageList = new ArrayList<>();
+        messageAdapter = new MessageAdapter(messageList);
+        recycler_view.setAdapter(messageAdapter);
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String question = et_msg.getText().toString().trim();
+                addToChat(question, Message.SENT_BY_ME);
+                et_msg.setText("");
+                callAPI(question);
+                tv_welcome.setVisibility(View.GONE);
+            }
+        });
+
+        client = new OkHttpClient().newBuilder()
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .readTimeout(60, TimeUnit.SECONDS)
+                .build();
+
+        // 퍼미션 체크
         if ( Build.VERSION.SDK_INT >= 23 ){
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
-                Manifest.permission.RECORD_AUDIO},PERMISSION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,
+                    Manifest.permission.RECORD_AUDIO},PERMISSION);
         }
 
         // xml의 버튼과 텍스트 뷰 연결
-        textView = (TextView)findViewById(R.id.contentsTextView1);
+        textView = (TextView)findViewById(R.id.tv_welcome);
         sttBtn = (ImageButton)findViewById(R.id.imageBtn);
 
         // RecognizerIntent 객체 생성
@@ -52,24 +121,41 @@ public class MainActivity4 extends AppCompatActivity {
             mRecognizer.setRecognitionListener(listener);
             mRecognizer.startListening(intent);
         });
+
     }
+
+    void addToChat(String message, String sentBy) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                messageList.add(new Message(message, sentBy));
+                messageAdapter.notifyDataSetChanged();
+                recycler_view.smoothScrollToPosition(messageAdapter.getItemCount());
+            }
+        });
+    }
+
     private RecognitionListener listener = new RecognitionListener() {
         @Override
         public void onReadyForSpeech(Bundle params) {
-            Toast.makeText(getApplicationContext(),"음성인식을 시작합니다.",Toast.LENGTH_SHORT).show();
+            Toast.makeText(getApplicationContext(), "음성인식을 시작합니다.", Toast.LENGTH_SHORT).show();
         }
 
         @Override
-        public void onBeginningOfSpeech() {}
+        public void onBeginningOfSpeech() {
+        }
 
         @Override
-        public void onRmsChanged(float rmsdB) {}
+        public void onRmsChanged(float rmsdB) {
+        }
 
         @Override
-        public void onBufferReceived(byte[] buffer) {}
+        public void onBufferReceived(byte[] buffer) {
+        }
 
         @Override
-        public void onEndOfSpeech() {}
+        public void onEndOfSpeech() {
+        }
 
         @Override
         public void onError(int error) {
@@ -117,16 +203,92 @@ public class MainActivity4 extends AppCompatActivity {
             ArrayList<String> matches =
                     results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
 
-            for(int i = 0; i < matches.size() ; i++){
+            for (int i = 0; i < matches.size(); i++) {
                 textView.setText(matches.get(i));
             }
         }
 
         @Override
-        public void onPartialResults(Bundle partialResults) {}
+        public void onPartialResults(Bundle partialResults) {
+        }
 
         @Override
-        public void onEvent(int eventType, Bundle params) {}
+        public void onEvent(int eventType, Bundle params) {
+        }
     };
-}
 
+    void addResponse(String response){
+        messageList.remove(messageList.size()-1);
+        addToChat(response, Message.SENT_BY_BOT);
+    }
+
+    void callAPI(String question) {
+        //okhttp
+        messageList.add(new Message("...", Message.SENT_BY_BOT));
+
+        //추가된 내용
+        JSONArray arr = new JSONArray();
+        JSONObject baseAi = new JSONObject();
+        JSONObject userMsg = new JSONObject();
+        try {
+            //AI 속성설정
+            baseAi.put("role", "user");
+            baseAi.put("content", "You are a helpful and kind AI Assistant.");
+            //유저 메세지
+            userMsg.put("role", "user");
+            userMsg.put("content", question);
+            //array로 담아서 한번에 보낸다
+            arr.put(baseAi);
+            arr.put(userMsg);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        JSONObject object = new JSONObject();
+        try {
+            //모델명 변경
+            object.put("model", "gpt-3.5-turbo");
+            object.put("messages", arr);
+//            아래 put 내용은 삭제하면 된다
+//            object.put("model", "text-davinci-003");
+//            object.put("prompt", question);
+//            object.put("max_tokens", 4000);
+//            object.put("temperature", 0);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestBody body = RequestBody.create(object.toString(), JSON);
+
+        Request request = new Request.Builder()
+                .url("https://api.openai.com/v1/chat/completions")  //url 경로 수정됨
+                .header("Authorization", "Bearer " + MY_SECRET_KEY)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                addResponse("Failed to load response due to " + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    JSONObject jsonObject = null;
+                    try {
+                        jsonObject = new JSONObject(response.body().string());
+                        JSONArray jsonArray = jsonObject.getJSONArray("choices");
+                        //아래 result 받아오는 경로가 좀 수정되었다.
+                        String result = jsonArray.getJSONObject(0).getJSONObject("message").getString("content");
+                        addResponse(result.trim());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    addResponse("Failed to load response due to " + response.body().string());
+                }
+            }
+        });
+    }
+}
