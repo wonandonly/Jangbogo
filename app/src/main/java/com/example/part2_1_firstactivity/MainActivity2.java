2273147e5a340ec408e00872eec17630701e8728
sm-koeunseo <koeunseo@sookmyung.ac.kr>
2023-05-23 21:57:58 +0900

    shop login check & auto login

--- app/src/main/java/com/example/part2_1_firstactivity/MainActivity2.java
+++ app/src/main/java/com/example/part2_1_firstactivity/MainActivity2.java
@@ -1,17 +1,29 @@
 package com.example.part2_1_firstactivity;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -42,8 +54,49 @@ public class MainActivity2 extends AppCompatActivity {
 
         FirebaseApp.initializeApp(getApplicationContext(), options, "secondary");
         secondaryFirestore = FirebaseFirestore.getInstance(FirebaseApp.getInstance("secondary"));
-        db= FirebaseFirestore.getInstance();
+        db= FirebaseFirestore.getInstance();    // shop
+
+        shopLogin();
     }
+
+    private void shopLogin(){
+        String jId = "t9S9FGgV7ygPMNAtX8Oj";
+
+        int status = NetworkStatus.getConnectivityStatus(getApplicationContext());
+        if (status == NetworkStatus.TYPE_MOBILE || status == NetworkStatus.TYPE_WIFI) {
+            Log.d(TAG, "wifi");
+
+            Task<QuerySnapshot> task = secondaryFirestore.collection("user")
+            .whereEqualTo("jId", jId)
+            .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            List list = new ArrayList();
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    Map map = document.getData();
+                                    map.put("key", document.getId());
+                                    list.add(map);
+                                    Log.d(TAG, "wifi" + map.get("shopId"));
+                                    // 쇼핑몰 로그인 하기!!
+                                }
+                            } else {
+                                Log.d(TAG, "Error getting documents: ", task.getException());
+                            }
+                        }
+                    });
+        }
+    }
+
+    /*@Override
+    public void onComplete(@NonNull Task task) {
+        Log.d(TAG, jId + "test");
+                    if(task.isSuccessful()알){
+                        QueryDocumentSnapshot document = (QueryDocumentSnapshot) task.getResult();
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                    }
+    }*/
+
 
     private void initData() {
         //nameEditText = findViewById(R.id.nameEditText);
